8c7505ef722e11017a334febc6b7aa3a7f094d7f
Wade Baize <wadebaize@Wades-MBP.jetpack>
2018-06-17 15:56:46 -0600

    finished Checkpoint 8 Buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -59,6 +60,14 @@ class Album extends Component {
         this.setState({ hovered: null});
       }
 
+      handlePrevClick(){
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max (0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+      }
+
   render() {
     return (
       <section className="album">
@@ -79,13 +88,6 @@ class Album extends Component {
             <tbody>
               {
                 this.state.album.songs.map( (song, index) =>
-<<<<<<< HEAD
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{song.title}</td>
-                    <td>{song.duration}</td>
-                  </tr>
-=======
                 <div>
                   <tr className="song" key={index.song} onClick={() => this.handleSongClick(song)} onMouseEnter={() => this.mouseHoverOn(song)} onMouseLeave={() => this.mouseHoverOff(song)}>
                                     <td> {this.buttonController(song, index)} </td>
@@ -93,11 +95,16 @@ class Album extends Component {
                                     <td>{song.duration}</td>
                   </tr>
                 </div>
->>>>>>> assignment-7-audio-playback
                 )
               }
             </tbody>
           </table>
+          <PlayerBar
+            isPlaying={this.state.isPlaying}
+            currentSong={this.state.currentSong}
+            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+            handlePrevClick={() => this.handlePrevClick()}
+            />
          </section>
     );
 }
